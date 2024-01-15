bfb3da84cffcf354db3f8e6068c6f2bdf84ccd39
akshaychavhan <akshay@rtledgers.com>
2024-01-15 22:42:05 +0530

    13- Quantity selection in product details screen completed

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,5 +1,6 @@
 'use client'
 import SetColor from '@/app/components/products/SetColor'
+import SetQuantity from '@/app/components/products/SetQuantity'
 import React, { useCallback, useState } from 'react'
 
 type Props = {
@@ -7,50 +8,68 @@ type Props = {
 }
 
 export type CartProductType = {
-    id:string,
-    name:string,
-    description:string,
-    catagory:string,
-    brand:string,
+    id: string,
+    name: string,
+    description: string,
+    catagory: string,
+    brand: string,
     selectedImg: SelectedImgType,
-    quantity:number,
-    price:number
+    quantity: number,
+    price: number
 }
 
 export type SelectedImgType = {
-    color:string,
-    colorCode:string,
-    image:string
+    color: string,
+    colorCode: string,
+    image: string
 }
 
 const Horizontal = () => {
-    return <hr className='w-[30% my-2]'/>
+    return <hr className='w-[30% my-2]' />
 }
 
 const ProductDetails = ({ product }: Props) => {
 
-    const [CartProduct,setCartProduct] = useState<CartProductType>({
-        id:product.id,
-        name:product.name,
-        description:product.description,
-        catagory:product.catagory,
-        brand:product.brand,
-        selectedImg: {...product.images[0]},
-        quantity:1,
-        price:product.price 
+    const [cartProduct, setCartProduct] = useState<CartProductType>({
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        catagory: product.catagory,
+        brand: product.brand,
+        selectedImg: { ...product.images[0] },
+        quantity: 1,
+        price: product.price
     })
     const productRating = product.reviews.reduce((acc: number, item: any) => {
         return acc + item.rating;
     }, 0) / product.reviews.length;
-    
-    console.clear();
-    console.log('CartProduct => ', CartProduct);
-    
-    const handColorSelect = useCallback((value:SelectedImgType)=>{
+
+    // console.clear();
+    // console.log('cartProduct => ', cartProduct);
+
+    const handColorSelect = useCallback((value: SelectedImgType) => {
         setCartProduct((prev) => {
-            return { ...prev,selectedImg: value}
+            return { ...prev, selectedImg: value }
         });
-    },[CartProduct.selectedImg]);
+    }, [cartProduct.selectedImg]);
+
+    const handleQtyDecrease = useCallback(() => {
+        if (cartProduct.quantity === 1) {
+            return;
+        }
+        setCartProduct((prev) => {
+            return { ...prev, quantity: --prev.quantity }
+        })
+    }, [cartProduct])
+
+    const handleQtyIncrease = useCallback(() => {
+        if (cartProduct.quantity === 99) {
+            return;
+        }
+        setCartProduct((prev) => {
+            return { ...prev, quantity: ++prev.quantity }
+        })
+    }, [cartProduct])
 
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
@@ -58,13 +77,13 @@ const ProductDetails = ({ product }: Props) => {
                 Images
             </div>
             <div
-            className='flex flex-col gap-1 text-slate-500 text-sm'>
+                className='flex flex-col gap-1 text-slate-500 text-sm'>
                 <h2
                     className='text-3xl font-medium text-slate-700'>
                     {product.name}
                 </h2>
-                <div 
-                className='flex items-center gap-2'>
+                <div
+                    className='flex items-center gap-2'>
                     {/* rating component */}
                     {productRating}
                     <div>{product.reviews.length}</div>
@@ -79,18 +98,21 @@ const ProductDetails = ({ product }: Props) => {
                     <span className='font-semibold'>BRAND:</span>
                     {product.brand}
                 </div>
-                <div className={product.inStock ? 'text-teal-400':'text-red-400'}>
-                    {product.inStock ? 'In stock': 'Out of Stock'}
+                <div className={product.inStock ? 'text-teal-400' : 'text-red-400'}>
+                    {product.inStock ? 'In stock' : 'Out of Stock'}
                 </div>
                 <Horizontal />
                 <SetColor
-                cartProduct={CartProduct}
-                images={product.images}
-                handColorSelect={handColorSelect}
+                    cartProduct={cartProduct}
+                    images={product.images}
+                    handColorSelect={handColorSelect}
                 />
-                <div>color</div>
                 <Horizontal />
-                <div>quantity</div>
+                <SetQuantity
+                    cartProduct={cartProduct}
+                    handleQtyDecrease={handleQtyDecrease}
+                    handleQtyIncrease={handleQtyIncrease}
+                />
                 <Horizontal />
                 <div>add to cart</div>
             </div>
